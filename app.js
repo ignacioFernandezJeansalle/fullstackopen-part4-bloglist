f9c8f05843070eb5f0268265b482e045e34b6cc6
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-06-12 20:17:46 -0300

    Create models/blog

--- app.js
+++ app.js
@@ -4,15 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = require("./models/blog");
 
 mongoose
   .connect(config.MONGODB_URI)
