bce5d6c15a1177bdae4605066c08426943f6d272
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-06-12 17:19:10 -0300

    Create utils/logger

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const logger = require("./utils/logger");
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -14,7 +15,10 @@ const blogSchema = new mongoose.Schema({
 const Blog = mongoose.model("Blog", blogSchema);
 
 const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
+mongoose
+  .connect(mongoUrl)
+  .then(() => logger.info("Connected to MongoDB"))
+  .catch((error) => logger.error("Error connecting to MongoDB: ", error.message));
 
 app.use(cors());
 app.use(express.json());
@@ -35,5 +39,5 @@ app.post("/api/blogs", (request, response) => {
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
