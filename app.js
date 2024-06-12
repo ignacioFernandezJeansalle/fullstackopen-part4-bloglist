593dd49f0708446747b538ab879986db48862282
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-06-12 20:47:18 -0300

    Create controllers/blogs

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
+const blogRouter = require("./controllers/blogs");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-const Blog = require("./models/blog");
+const mongoose = require("mongoose");
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -14,18 +14,6 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogRouter);
 
 module.exports = app;
