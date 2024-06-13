29f450fc8cc523963d17741a057926243a21c13d
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-06-12 21:15:25 -0300

    Create utils/middleware

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const blogRouter = require("./controllers/blogs");
-const config = require("./utils/config");
-const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
 const mongoose = require("mongoose");
 
 mongoose
@@ -13,7 +14,11 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
---
+++ utils/middleware.js
@@ -0,0 +1,29 @@
+const logger = require("../utils/logger");
+
+const requestLogger = (req, res, next) => {
+  logger.info("-----");
+  logger.info("Method:", req.method);
+  logger.info("Path:  ", req.path);
+  logger.info("Body:  ", req.body);
+  logger.info("-----");
+  next();
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "Unknown endpoint" });
+};
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message);
+
+  if (error.name === "CastError") return response.status(400).send({ error: "malformatted id" });
+  if (error.name === "ValidationError") return response.status(400).json({ error: error.message });
+
+  next(error);
+};
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+};
