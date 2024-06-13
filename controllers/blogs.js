f26f72f0cb096dc049c41ebf595737b8c6aeafa0
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-06-12 21:15:09 -0300

    Add catch error a next function

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,18 +1,23 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogRouter.get("/", (request, response, next) => {
+  Blog.find({})
+    .then((blogs) => {
+      response.json(blogs);
+    })
+    .catch((error) => next(error));
 });
 
-blogRouter.post("/", (request, response) => {
+blogRouter.post("/", (request, response, next) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
 });
 
 module.exports = blogRouter;
