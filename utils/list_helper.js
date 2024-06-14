07558e992ae24900227742437e2e4c8df9520284
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-06-14 14:42:06 -0300

    Create mostBlogs test without lodash library (ex4.6)

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -16,8 +16,28 @@ const favoriteBlog = (blogs) => {
   return blogs.length === 0 ? { message: "Empty blogs" } : blogs.reduce(reducer, {});
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return { message: "Empty blogs" };
+
+  const blogsByAuthor = blogs.reduce((result, blog) => {
+    result[blog.author] = result[blog.author] || 0;
+    result[blog.author] += 1;
+
+    return result;
+  }, []);
+
+  let mostBlogs = { blogs: 0 };
+
+  for (const [author, blogs] of Object.entries(blogsByAuthor)) {
+    mostBlogs = mostBlogs.blogs < blogs ? { author, blogs } : mostBlogs;
+  }
+
+  return mostBlogs;
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
