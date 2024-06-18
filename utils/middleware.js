15c2cd57fbe024c3f04c513d418b503257871a2c
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-06-18 10:46:32 -0300

    Fix error tag response => res

--- utils/middleware.js
+++ utils/middleware.js
@@ -16,8 +16,8 @@ const unknownEndpoint = (req, res) => {
 const errorHandler = (error, req, res, next) => {
   logger.error(error.message);
 
-  if (error.name === "CastError") return response.status(400).send({ error: "malformatted id" });
-  if (error.name === "ValidationError") return response.status(400).json({ error: error.message });
+  if (error.name === "CastError") return res.status(400).send({ error: "malformatted id" });
+  if (error.name === "ValidationError") return res.status(400).json({ error: error.message });
 
   next(error);
 };
