e1c7a71abe392e45a46e12381b038675a455678f
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-07-10 16:11:48 -0300

    Create loginRouter

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require("./utils/logger");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const loginRouter = require("./controllers/login");
 const usersRouter = require("./controllers/users");
 const blogsRouter = require("./controllers/blogs");
 const middleware = require("./utils/middleware");
@@ -17,6 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/login", loginRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
 
