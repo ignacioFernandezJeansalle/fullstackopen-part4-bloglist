3762351d3525a9a5cb736bfe1ea50a903f329e60
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-07-12 10:39:24 -0300

    Add token validation tfor put blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -40,11 +40,16 @@ blogsRouter
 
 blogsRouter
   .route("/:id")
-  .put(async (request, response, next) => {
+  .put(middleware.userExtractor, async (request, response, next) => {
     const { id } = request.params;
     const { likes } = request.body;
 
     try {
+      const user = request.user;
+      const blog = await Blog.findById(id);
+
+      if (user._id.toString() !== blog.user.toString()) return response.status(401).json({ error: "user invalid" });
+
       const updatedBlog = await Blog.findByIdAndUpdate(id, { likes }, { new: true });
       response.json(updatedBlog);
     } catch (error) {
